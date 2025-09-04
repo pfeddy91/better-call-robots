d8202f9bf7c6bb7e4e5246543290f11fef2e6912
pfeddy91 <pfedele91@gmail.com>
2025-09-04 18:26:34 +0100

    Web: de-duplicate cn export in src/lib/utils.ts

--- apps/web/src/lib/utils.ts
+++ apps/web/src/lib/utils.ts
@@ -5,9 +5,4 @@ export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
 
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
-
-export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
-}
+ 
